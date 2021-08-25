3be9d04abd8dc1972169863a8994fc2b5e1f917c
santiagolaiton <santiago.laitoncubides@hotmail.com>
2021-08-24 23:00:32 -0500

    agregando punto uno

---
+++ lab01ParteDos/src/main/java/eci/edu/ieti/lab01/lab01ParteDos/Lab01ParteDosApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.ieti.lab01.lab01ParteDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01ParteDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab01ParteDosApplication.class, args);
+	}
+
+}
---
+++ lab01ParteDos/src/test/java/eci/edu/ieti/lab01/lab01ParteDos/Lab01ParteDosApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.ieti.lab01.lab01ParteDos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01ParteDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
